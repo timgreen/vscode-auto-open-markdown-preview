d57130c25ba44a47ac3d6b90aa27d5f8dbdcbc11
Tim Green <iamtimgreen@gmail.com>
2019-05-15 12:09:56 +1000

    Open enhanced preview instead.

--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,46 @@
 'use strict';
 import * as vscode from 'vscode';
-import {workspace, window, commands, ExtensionContext} from 'vscode';
+import { commands, ExtensionContext, window } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-    let alreadyOpenedFirstMarkdown = false;
-    let markdown_preview_command_id = "";
-    let close_other_editor_command_id = "";
-    close_other_editor_command_id = "workbench.action.closeEditorsInOtherGroups";
-    markdown_preview_command_id = "markdown.showPreviewToSide";
-    function previewFirstMarkdown() {
-        if (alreadyOpenedFirstMarkdown) {
-	    return;
-	}
-        let editor = window.activeTextEditor;
-        if (editor) {
-            let doc = editor.document;
-            if (doc && doc.languageId === "markdown") {
-                openMarkdownPreviewSideBySide();
-                alreadyOpenedFirstMarkdown = true;
-            }
-        }
+  let alreadyOpenedFirstMarkdown = false;
+  let markdown_preview_command_id = '';
+  let close_other_editor_command_id = '';
+  close_other_editor_command_id = 'workbench.action.closeEditorsInOtherGroups';
+  markdown_preview_command_id = 'markdown-preview-enhanced.openPreviewToTheSide';
+  function previewFirstMarkdown() {
+    if (alreadyOpenedFirstMarkdown) {
+      return;
     }
-    function openMarkdownPreviewSideBySide() {
-        commands.executeCommand(close_other_editor_command_id)
-        .then(() => commands.executeCommand(markdown_preview_command_id))
-        .then(() => {}, (e) => console.error(e));
+    let editor = window.activeTextEditor;
+    if (editor) {
+      let doc = editor.document;
+      if (doc && doc.languageId === 'markdown') {
+        openMarkdownPreviewSideBySide();
+        alreadyOpenedFirstMarkdown = true;
+      }
     }
+  }
+  function openMarkdownPreviewSideBySide() {
+    commands
+      .executeCommand(close_other_editor_command_id)
+      .then(() => commands.executeCommand(markdown_preview_command_id))
+      .then(() => {}, e => console.error(e));
+  }
 
-    if (window.activeTextEditor) {
-        previewFirstMarkdown();
-    } else {
-        vscode.window.onDidChangeActiveTextEditor(()=>{
-            previewFirstMarkdown();
-        });
-    }
-
-    vscode.workspace.onDidOpenTextDocument((doc)=>{
-        if (doc && doc.languageId === "markdown") {
-            openMarkdownPreviewSideBySide();
-        }
+  if (window.activeTextEditor) {
+    previewFirstMarkdown();
+  } else {
+    vscode.window.onDidChangeActiveTextEditor(() => {
+      previewFirstMarkdown();
     });
+  }
+
+  vscode.workspace.onDidOpenTextDocument(doc => {
+    if (doc && doc.languageId === 'markdown') {
+      openMarkdownPreviewSideBySide();
+    }
+  });
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
